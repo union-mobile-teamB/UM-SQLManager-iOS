0a673b4aee8eea4f3e1d16ce48cbe8147221799f
HyeonJun Kang <khjj9865@naver.com>
2024-09-27 12:09:15 +0900

    add: NSObjectConvertable

--- Sources/UM-SQLManager-iOS/Protocol/NSObjectConvertible.swift
+++ Sources/UM-SQLManager-iOS/Protocol/NSObjectConvertible.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol NSObjectConvertible {
+    func toNSObject() -> NSObject
+}
+
+extension Int: NSObjectConvertible {
+    func toNSObject() -> NSObject {
+        return self as NSObject
+    }
+}
+
+extension Double: NSObjectConvertible {
+    func toNSObject() -> NSObject {
+        return self as NSObject
+    }
+}
+
+extension String: NSObjectConvertible {
+    func toNSObject() -> NSObject {
+        return self as NSObject
+    }
+}
+
+extension Bool: NSObjectConvertible {
+    func toNSObject() -> NSObject {
+        return self as NSObject
+    }
+}
